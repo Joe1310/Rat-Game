cf020dcc5b2254c0c40e0e2639edbb8b4776b341
HorridJoe <joseph1310@Sky.com>
2021-11-17 13:52:55 +0000

    Basic constructor for Tile class added

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,18 @@
 public class Tile {
+    private String tileType;
+    private int x;
+    private int y;
+    private String[] movementOptions;
+
+    public Tile(String tileType, int x, int y){
+        this.x = x;
+        this.y = y;
+        this.tileType = tileType;
+        movementOptions = getMovementOptions();
+    }
+
+    public static String[] getMovementOptions(){
+        return null;
+    }
+
 }
