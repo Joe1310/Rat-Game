99f7ffcc7d95bc4285c5263bcf8e2e4d315b08b4
HorridJoe <joseph1310@Sky.com>
2021-11-17 13:58:27 +0000

    Blank javadoc comments added

--- src/Tile.java
+++ src/Tile.java
@@ -1,9 +1,17 @@
+/**
+ *
+ */
 public class Tile {
     private String tileType;
     private int x;
     private int y;
     private String[] movementOptions;
 
+    /**
+     * @param tileType The type of tile (Grass, Tunnel, Path).
+     * @param x x coordinate of the tile.
+     * @param y y coordinate of the tile.
+     */
     public Tile(String tileType, int x, int y){
         this.x = x;
         this.y = y;
@@ -11,6 +19,9 @@ public class Tile {
         movementOptions = getMovementOptions();
     }
 
+    /**
+     * @return return a list of movement options from that tile.
+     */
     public static String[] getMovementOptions(){
         return null;
     }
