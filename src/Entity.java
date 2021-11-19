8a4510d9bc3cfd4a5d8185d1d016fd0d6d0e3cf1
ElliotMk1 <42278673+ElliotMk1@users.noreply.github.com>
2021-11-19 14:35:36 +0000

    Update Entity.java

--- src/Entity.java
+++ src/Entity.java
@@ -1,5 +1,12 @@
-public class Entity {
-    public Entity(){
-
+public abstract class Entity {
+	
+	protected int[] location;
+	private Object entityType;
+	
+    public Entity(Object obj, int[] location){
+    	entityType = obj;
+    	this.location = location;
     }
+    
+    public abstract void draw();
 }
