976f15efd4911803d05c29cae858c38b3163818b
Tennoats <tennoats@gmail.com>
2021-11-19 15:32:07 +0000

    Updated Item.java
    Added functionality to Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,27 @@
 public class Item extends Entity{
-    public Item(){
+    private int quantity;
+    private int spawnRate;
 
+    /**
+     *
+     * @param quantity The number of this item
+     * @param spawnRate The rate at which this item is increased
+     */
+    private Item(int quantity, int spawnRate) {
+        this.quantity = quantity;
+        this.spawnRate = spawnRate;
     }
+
+    private void increaseItem() {
+        this.quantity += 1;
+    }
+
+    public void useItem() {
+        this.quantity -= 1;
+    }
+
+    public int getItems() {
+        return this.quantity;
+    }
+
 }
