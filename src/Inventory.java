75791ef5117034d63aa5ac79d496bf71bb122c6e
Tennoats <tennoats@gmail.com>
2021-11-22 14:44:26 +0000

    Updated Inventory.java to include add functions to add 1 to the quantity of an item

--- src/Inventory.java
+++ src/Inventory.java
@@ -18,9 +18,13 @@ public class Inventory {
     }
 
     // functions get'ItemName' returns the amount of them in the inventory
+    // functions add'ItemName' adds one to the amount of items
     // functions remove'ItemName' takes one off the amount of them
     public int getGas() {
         return gasQuantity;
+    }
+    public void addGas() {
+        this.gasQuantity += 1;
     }
     public void removeGas() {
         this.gasQuantity -= 1;
@@ -30,6 +34,9 @@ public class Inventory {
     public int getDeathRat() {
         return deathRatQuantity;
     }
+    public void addDeathRat() {
+        this.deathRatQuantity += 1;
+    }
     public void removeDeathRat() {
         this.deathRatQuantity -= 1;
     }
@@ -37,6 +44,9 @@ public class Inventory {
 
     public int getPoison() {
         return poisonQuantity;
+    }
+    public void addPoison() {
+        this.poisonQuantity += 1;
     }
     public void removePoison() {
         this.poisonQuantity -= 1;
@@ -46,6 +56,9 @@ public class Inventory {
     public int getSterilization() {
         return sterilizationQuantity;
     }
+    public void addSterilization() {
+        this.sterilizationQuantity += 1;
+    }
     public void removeSterilization() {
         this.sterilizationQuantity -= 1;
     }
@@ -53,6 +66,9 @@ public class Inventory {
 
     public int getSexChange() {
         return sexChangeQuantity;
+    }
+    public void addSexChange() {
+        this.sexChangeQuantity += 1;
     }
     public void removeSexChange() {
         this.sexChangeQuantity -= 1;
@@ -62,6 +78,9 @@ public class Inventory {
     public int getNoEntrySign() {
         return noEntrySignQuantity;
     }
+    public void addNoEntrySign() {
+        this.noEntrySignQuantity += 1;
+    }
     public void removeNoEntrySign() {
         this.noEntrySignQuantity -= 1;
     }
@@ -69,6 +88,9 @@ public class Inventory {
 
     public int getBomb() {
         return bombQuantity;
+    }
+    public void addBomb() {
+        this.bombQuantity += 1;
     }
     public void removeBomb() {
         this.bombQuantity -= 1;
