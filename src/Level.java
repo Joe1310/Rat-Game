4effc46ec683c27073c16ab14575f5a5f63ed8df
SamarthyaPatel <86912122+SamarthyaPatel@users.noreply.github.com>
2021-11-24 03:02:28 +0000

    Rebuilt methods
    Methods are rebuilt and data files are upgraded to new formats.
    Check new data files and refer to TextFileInformation.txt to understand data files.

--- src/Level.java
+++ src/Level.java
@@ -4,124 +4,153 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-//File extends Map class
-public class Level {
 
-	//Variables 
+public class Level {
 	
-	int levelNumber;
-	//Map levelMap;
+	//Variables
+	int parTime;
+	int remainingTime = 0;
+	int maxRats;
+	Tile[][] tileLayout;
+
+	int itemSpawnRate;
+	int[] entityLocations;
+	
 	int[] inventory;
-	int levelScore;
+	int playerScore;
+	int levelScore = 0;
 	
-	public static void main(String[] args) {
-		
-		Levelinfo("levelInfo.txt");
-		
+	
+	//Constructor
+	public Level(int parTime, int maxRats, Tile[][] tileLayout) {
+		this.tileLayout = tileLayout;
+		this.parTime = parTime;
+		this.maxRats = maxRats;
 	}
 
-	public static void Levelinfo(String fn) {
+	//Method reads file and calls appropriate function while passing scanner to it
+	public void readFile(String file) {
 		
-		File filename = new File(fn);
-		
-		//Scanner
+		File fileName = new File(file);
 		Scanner scan = null;
-
 		try {
-
-			scan = new Scanner(filename);
-
+			scan = new Scanner(fileName);
+			
 		} catch (FileNotFoundException e) {
-			System.out.println("Could not find "+filename);
+			System.out.println("Could not find "+fileName);
 			e.printStackTrace();
 		}
 		
-		System.out.println(readLevelData(scan));
+		//Condition for passing scanner to the appropriate method
+		if(file.contains("level")) {
+			readLevel(scan);
+		} else {
+			readGame(scan);
+		}
 		
 	}
 	
-	public static String readLevelData(Scanner scan) {
+	//Method reads Level data and creates level objects
+	public void readLevel(Scanner scan) {
 		
-		//Variables
-		int column;
-		int row;
-		String[] grid;
-		String[] rats;
-		int rat;
-		String items;
 		int maxRats;
-		String levelData = "";
-		int parTime; //seconds
-		int itemSpawnRate; //seconds;
-		
-		column = scan.nextInt();
-		row = scan.nextInt();
-		grid = new String[row];
-		
-		for (int i = 0; i < row; i ++) {
-			grid[i] = scan.next();
-		}
-		
-		rat = scan.nextInt();
-		
-		rats = new String[rat];
-		for(int i = 0; i < rat; i ++) {
-			rats[i] = scan.next();
-		}
-		
-		items = scan.next();
-		
-		maxRats = scan.nextInt();
+		int row, column;
+		char[][] tileLayout;
 		
 		parTime = scan.nextInt();
+		maxRats = scan.nextInt();
+		row = scan.nextInt();
+		column = scan.nextInt();
 		
-		itemSpawnRate = scan.nextInt();
-		
-		levelData += "Column: " + column + "\nRow: " + row + "\nGrid: \n";
-		for(String str : grid) {
-			levelData += str + "\n";
-		}
-		levelData += "\nAlive Rats: " + rat + "\nRats: \n";
-		for(String str : rats) {
-			levelData += str + "\n";
-		}
-		levelData += "\nItems: " + items + "\nMaximum Rats: " + maxRats +
-				"\nParTime: " + parTime + "\nItem Spawn Rate: " + itemSpawnRate;
-		
-		return levelData;
-	}
-	
-	
-	public static void constructMap() {
-		
-		//This method uses JavaFx to construct the Map.
-	}
-	
-	
-	public void saveCurrent() {
-		//We might need playerID for storing different players ongoing game
-		try {
-			FileWriter saveGame = new FileWriter("savedGame.txt");
-			saveGame.write("Level Layout");
-			saveGame.write("List of rats and their properties");
-			
-			for(int i = 0; i < inventory.length; i++) {
-				saveGame.write(inventory[i] + " ");
+		tileLayout = new char[row][column];
+		for (int i = 0; i < row; i ++) {
+			String str = scan.next();
+			for (int j = 0; j < column; j ++) {
+				tileLayout[i][j] = str.charAt(j);
 			}
+		}
+		
+		//Error: tileLayout is Tile type but while reading text it is char type
+		Level level = new Level((parTime - remainingTime), maxRats, tileLayout);
+	}
+	
+	//Method reads Game data using Scanner and creates objects
+	public void readGame(Scanner scan) {
+		
+		int aliveRats;
+		String sex;
+		int x, y;
+		int health;
+		String direction;
+		boolean sterile, isPregnant;
+		String inventory;
+		int levelNumber;
+		
+		this.remainingTime = scan.nextInt();
+		
+		aliveRats = scan.nextInt();
+		
+		for (int i = 0; i < aliveRats; i ++) {
 			
-			saveGame.write("parTime");
-			saveGame.write("itemSpawnRate");
-			saveGame.write("PlayerID and Score");
+			sex = scan.next();
+			x = scan.nextInt();
+			y = scan.nextInt();
+			int[] location = {x,y};
+			health = scan.nextInt();
+			direction = scan.next();
+			sterile = scan.nextBoolean();
+			isPregnant = scan.nextBoolean();
+			
+			Entity rat = new AdultRat(health, 5, sterile, location, direction, 1, sex, isPregnant);
+		}
+		
+		inventory = scan.next();
+		Inventory item = new Inventory(inventory.charAt(0), 
+				inventory.charAt(1), inventory.charAt(2), 
+				inventory.charAt(3), inventory.charAt(4), 
+				inventory.charAt(5), inventory.charAt(6));
+		
+		for(int i = 0; i < 7; i++) {
+			this.inventory[i] = inventory.charAt(i);
+		}
+		
+		this.playerScore = scan.nextInt();
+		levelNumber = scan.nextInt();
+		
+		//Makes level object
+		readFile(levelNumber + "level.txt");
+		
+	}
+
+	//Method saves the ongoing Game using playerID
+	public void saveCurrent(String playerID) {
+		
+		String saveFile = playerID + "savedGame.txt";
+		
+		try {
+			
+			FileWriter saveGame = new FileWriter(saveFile);
+			saveGame.write(remainingTime + "\n");
+			saveGame.write("Alive Rats number\n <List> \n");
+			saveGame.write("<Inventory>\n");
+			saveGame.write(playerScore);
+			saveGame.write("\n<levelNumber>");
 			saveGame.close();
-			System.out.println("Game saved successfully.");
-			
 			
 		} catch (IOException e) {
-			System.out.println("An error occurred while saving the current game.");
+			
+			System.out.println("\nAn error occurred while saving the current game.");
 			e.printStackTrace();
-		}
-		
-		
+			
+		}	
+	}
+
+	//This method creates map object and returns the object
+	public Map constructMap() {
+
+		Map levelMap = new Map(tileLayout, itemSpawnRate, entityLocations, maxRats);
+
+		return levelMap;
 	}
 
 }
