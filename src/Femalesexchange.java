30db9c00209819933cc7d5358289edffc41270d9
shivraj-stk <79634932+shivraj-stk@users.noreply.github.com>
2021-11-24 17:10:15 +0000

    Add files via upload

---
+++ src/Femalesexchange.java
@@ -0,0 +1,14 @@
+public class Femalesexchange extends AdultRat {
+	
+	public Femalesexchange(int[] location) {
+		super(location, "Femalesexchange.png");
+	}
+	
+	public void act() {
+		char sx = getsex()
+		if sx == 'm'{
+				setsex('f')
+		//need to create a setter function in class AdultRat as sex is a private data member		
+		}
+	}
+}	
