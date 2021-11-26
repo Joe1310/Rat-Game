b03e45c22d0806ade7ce6f46777e363bd5434256
HorridJoe <joseph1310@Sky.com>
2021-11-26 15:48:00 +0000

    MaleSexChange readded

---
+++ src/MaleSexChange.java
@@ -0,0 +1,11 @@
+public class MaleSexChange extends Entity {
+
+    public MaleSexChange(int[] location) {
+        super(location, "MSexChange.png");
+    }
+
+    public void act() {
+
+        //need to create a setter function in class AdultRat as sex is a private data member
+    }
+}
