830d7321dfa745e7b951304189c37307024d2eb0
rajsarky <help.rajsarkar@gmail.com>
2021-11-26 16:12:27 +0000

    Location Updated in the Rat();
    -rajsarky

--- src/BabyRat.java
+++ src/BabyRat.java
@@ -33,6 +33,6 @@ public class BabyRat extends Rat {
     }
 
     public String toString(){
-        return "b" + " " + sterile + " " + directionFacing + " " + age + " " + health;
+        return "b" + this.location[0] + " " + this.location[1] + " " +  sterile + " " + directionFacing + " " + age + " " + health;
     }
 }
