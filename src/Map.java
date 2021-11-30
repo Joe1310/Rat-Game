66e10db55f2763766a5e2f2ee38718ad710f1aa6
jayparfitt <parfitt2001@gmail.com>
2021-11-30 00:59:38 +0000

    Map method to draw tileLayout(not 100% done yet)

--- src/Map.java
+++ src/Map.java
@@ -1,32 +1,70 @@
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Map {
-    private int itemSpawnRate;
-    private int[] entityLocations;
-    private int maxRat;
-    public static Tile[][] tileLayout;
+ //   int itemSpawnRate;
+//    int[] entityLocations;
+
+    int maxRat;
+    static Tile[][] tileLayout;
+    public int tick;
+    private Canvas canvas;
 
  //   public static void main(String[] args) {
- //       entityTick();
- //   }
+   // }
+
 
     /**
      * @param tileLayout is a text file containing the tile layout
-     * @param itemSpawnRate has item spawn rate
-     * @param entityLocations has the locations of both rats and items in entity
      */
-    public Map(Tile[][] tileLayout,int itemSpawnRate,
-               int[] entityLocations, int maxRat) {
+    public Map(Tile[][] tileLayout, int maxRat) {
 
-
-        Map.tileLayout = tileLayout;
-        this.itemSpawnRate = itemSpawnRate;
-        this.entityLocations = entityLocations;
+        this.tileLayout = tileOut(tileLayout);
         this.maxRat = maxRat;
+        tick = entityTick();
     }
+
+    public Tile[][] tileOut(Tile[][] x) {
+        Image grassImage;
+        Image dirtImage;
+        Image tunnelImage;
+
+        final int GRID_CELL_WIDTH = 50;
+        final int GRID_CELL_HEIGHT = 50;
+
+        grassImage = new Image("GrassTile(1).png");
+        tunnelImage = new Image("Tunnel.png");
+        dirtImage = new Image("Dirt.png");
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        int tempY = 0;
+
+        for (int i = 0; i < (x.length); i++) {
+            for (int j = 0; j < x.length; j++) {
+                Image tileType = null;
+                if (x[i][j].getTileType() == 'G') {
+                    tileType = grassImage;
+                } else if (x[i][j].getTileType() == 'T') {
+                    tileType = tunnelImage;
+                } else if (x[i][j].getTileType() == 'D') {
+                    tileType = dirtImage;
+                }
+                gc.drawImage(tileType, i * GRID_CELL_WIDTH, tempY * GRID_CELL_HEIGHT);
+                tempY += 1;
+            }
+        }
+        System.out.println(Arrays.toString(x));
+        return x;
+    }
+
 
     /**
      *
@@ -38,7 +76,6 @@ public class Map {
         exec.scheduleWithFixedDelay(new Runnable() {
             private int tick = 0;
 
-            @Override
             public void run() {
                 tick++;
                 System.out.println(tick);
@@ -63,5 +100,9 @@ public class Map {
             movementOptions.add("s");
         }
         return movementOptions;
+    }
+
+    public String toString () {
+        return tileLayout.toString();
     }
 }
