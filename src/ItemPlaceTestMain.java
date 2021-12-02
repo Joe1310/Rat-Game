c813262a0fafd97599e1186b569d870e01f57820
Tennoats <tennoats@gmail.com>
2021-12-02 19:19:38 +0000

    pushing so i can pull
    says it all really

--- src/ItemPlaceTestMain.java
+++ src/ItemPlaceTestMain.java
@@ -10,7 +10,12 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.*;
+import javafx.scene.image.*;
 import javafx.stage.Stage;
+
+import java.util.EventListener;
+import java.util.ArrayList;
 
 
 public class ItemPlaceTestMain extends Application{
@@ -31,9 +36,32 @@ public class ItemPlaceTestMain extends Application{
 		// Build the GUI 
 		Pane root = buildGUI();		
 		
+		Image image8 = new Image("Bomb.png");
+		ImageView img8 = new ImageView(image8);
+		img8.setX(100);
+		img8.setY(100);
+		
+		root.getChildren().addAll(img8);
+		
 		// Create a scene from the GUI
 		Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+		
+		img8.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent event) {	
+				img8.setX((int)event.getSceneX());
+				img8.setY((int)event.getSceneY());
+			}
+		});
 			
+		img8.setOnMouseReleased(new EventHandler<MouseEvent>() {		
+			public void handle(MouseEvent event) {
+				img8.setX(100);
+				img8.setY(100);
+				Entity bomb = new Bomb(getMouseLocation(event));
+				bomb.draw();
+			}
+		});
+		
 		// Display the scene on the stage
 		primaryStage.setScene(scene);
 		primaryStage.show();	
@@ -53,29 +81,51 @@ public class ItemPlaceTestMain extends Application{
 		canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 		root.setCenter(canvas);
 		
-		// Create a siderbar with some nice padding and spacing
+		// Create a sidebar with some nice padding and spacing
 		VBox sidebar = new VBox();
 		sidebar.setSpacing(10);
-		sidebar.setPadding(new Insets(50, 10, 0, 0)); 
+		sidebar.setPadding(new Insets(0, 10, 0, 0)); 
 		root.setRight(sidebar);
 		
-		Button gasButton = new Button("Gas");
-		Button deathRatButton = new Button("Death Rat");
-		Button poisonButton = new Button("Poison");
-		Button sterilizationButton = new Button("Sterilization");
-		Button femaleSexChangeButton = new Button("Female Sex change");
-		Button maleSexChangeButton = new Button("Male Sex Change");
-		Button noEntrySignButton = new Button("No Entry Sign");
-		Button bombButton = new Button("Bomb");
+		
+		Image image = new Image("Gas2.png");
+		ImageView img = new ImageView(image);
+		
+		Image image1 = new Image("DeathRat.png");
+		ImageView img1 = new ImageView(image1);
+		
+		Image image2 = new Image("Poison.png");
+		ImageView img2 = new ImageView(image2);
+		
+		Image image3 = new Image("Sterilization.png");
+		ImageView img3 = new ImageView(image3);
+		
+		Image image4 = new Image("FemaleSexChange.png");
+		ImageView img4 = new ImageView(image4);
+		
+		Image image5 = new Image("MaleSexChange.png");
+		ImageView img5 = new ImageView(image5);
+		
+		Image image6 = new Image("NoEntrySign.png");
+		ImageView img6 = new ImageView(image6);
+		
+		Image image7 = new Image("Bomb.png");
+		ImageView img7 = new ImageView(image7);
+		
+		
+		Button gasButton = new Button("", img);
+		Button deathRatButton = new Button("", img1);
+		Button poisonButton = new Button("", img2);
+		Button sterilizationButton = new Button("", img3);
+		Button femaleSexChangeButton = new Button("", img4);
+		Button maleSexChangeButton = new Button("", img5);
+		Button noEntrySignButton = new Button("", img6);
+		Button bombButton = new Button("", img7);
 		
 		sidebar.getChildren().addAll(gasButton, deathRatButton, poisonButton, sterilizationButton, femaleSexChangeButton, maleSexChangeButton, noEntrySignButton, bombButton);
 		
-		
 		gasButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
 			public void handle(MouseEvent event) {
-				
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-
 				Entity gas = new Gas(getMouseLocation(event), false);
 				gas.draw();
 			}
@@ -83,9 +133,6 @@ public class ItemPlaceTestMain extends Application{
 		
 		deathRatButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
 			public void handle(MouseEvent event) {
-				
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-
 				Entity deathRat = new DeathRatSpawner(getMouseLocation(event));
 				deathRat.draw();
 			}
@@ -93,20 +140,20 @@ public class ItemPlaceTestMain extends Application{
 		
 		poisonButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
 			public void handle(MouseEvent event) {
-				
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-
 				Entity poison = new Poison(getMouseLocation(event));
 				poison.draw();
 			}
 		});
-
+		
+		sterilizationButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
+			public void handle(MouseEvent event) {
+				Entity sterilization = new Sterilization(getMouseLocation(event));
+				sterilization.draw();
+			}
+		});
 
 		femaleSexChangeButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
 			public void handle(MouseEvent event) {
-				
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-
 				Entity femaleSexChange = new FemaleSexChange(getMouseLocation(event));
 				femaleSexChange.draw();
 			}
@@ -114,9 +161,6 @@ public class ItemPlaceTestMain extends Application{
 		
 		maleSexChangeButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
 			public void handle(MouseEvent event) {
-				
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-
 				Entity maleSexChange = new MaleSexChange(getMouseLocation(event));
 				maleSexChange.draw();
 			}
@@ -124,9 +168,6 @@ public class ItemPlaceTestMain extends Application{
 		
 		noEntrySignButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
 			public void handle(MouseEvent event) {
-				
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-
 				Entity noEntrySign = new NoEntrySign(getMouseLocation(event));
 				noEntrySign.draw();
 			}
@@ -134,13 +175,11 @@ public class ItemPlaceTestMain extends Application{
 		
 		bombButton.setOnMouseReleased(new EventHandler<MouseEvent>() {		
 			public void handle(MouseEvent event) {
-				
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-
 				Entity bomb = new Bomb(getMouseLocation(event));
 				bomb.draw();
 			}
 		});
+		
 		
 		return root;
 	}
